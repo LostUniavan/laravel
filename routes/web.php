814c6b5d0b4652109ab1a675409e951d15c711a3
LostUniavan <joao.rocha@uniavan.edu.br>
2024-02-24 16:16:08 -0300

    pushing

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/calculator', function () {
     return view('welcome');
 });
-
-Route::get('ola/{text}', function ($text) {
-    echo "Olá, " . $text;
-});
